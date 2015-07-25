2f292b7be9be2c4b3d1bd68d3afb43e48a1ce3c7
Melissa Tian <melissa@dewtouch.com>
2015-07-26 04:39:03 +0800

    Finished Reminder

--- js/modules/voca/VocaCtrl.js
+++ js/modules/voca/VocaCtrl.js
@@ -1,11 +1,11 @@
 app.controller('VocaCtrl', function($rootScope, $scope,$http, $interval, SETTINGS, INFO){
 	console.info('VocaCtrl started @ ' + $rootScope.startTimestamp);
 
+    jQuery('#datetimepicker1').datetimepicker();
 
     $scope.custom = true;
     $scope.toggleCustom = function() {
         $scope.custom = $scope.custom === false ? true: false;
     };
-
 
 });
