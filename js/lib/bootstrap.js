c1ce863c23ada10410dcd6c2b31e40db24ba04eb
xuxichen <xuxi@dewtouch.com>
2015-07-26 08:14:26 +0800

    text change

--- js/lib/bootstrap.js
+++ js/lib/bootstrap.js
@@ -65,7 +65,7 @@ app.config(['$translateProvider', function ($translateProvider) {
         VOCA_RECORD          : 'RECORD',
         VOCA_SENT            : 'VOICE MEMO SENT',
         CONT_CONTACTS_LIST   : 'Contacts List',
-        CONT_MY_SON          : 'My Son',
+        CONT_MY_SON          : 'Son',
         CONT_SET_DEFAULT     : 'Set Default',
         CONT_CALL            : 'Call',
         REVE_RECORDING	     : 'Record Voice',
