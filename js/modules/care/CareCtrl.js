25aae004b09e1d41e47bed2068223640abf9964f
Melissa Tian <melissa@dewtouch.com>
2015-07-26 10:36:16 +0800

    Edited images

--- js/modules/care/CareCtrl.js
+++ js/modules/care/CareCtrl.js
@@ -87,18 +87,18 @@ app.controller('CareCtrl', function($rootScope, $scope, $http, $interval, SETTIN
 			{ name : 'Tang Xin Yi',    distance: 5.0, location:'36 Keong Saik Road', icon:'/img/avatar/XY.jpg', desc:'' },
 			{ name : 'Ashley Ng',      distance: 5.0, location:'28 Maxwell Road', icon:'/img/avatar/Ash.jpg', desc:'' }
 		],
-		hospitals : [
-			{ name : 'AgeWell Artsz', 							distance: 5.0, location:'', icon:'/img/hospitals/image1.jpg', desc:'' },
-			{ name : 'Assist Care', 							distance: 5.0, location:'', icon:'/img/hospitals/image2.jpg', desc:'' },
-			{ name : 'Handicaps Welfare Association (Whampoa)', distance: 5.0, location:'', icon:'/img/hospitals/image3.jpg', desc:'' },
-			{ name : 'Lotus Eldercare Pte Ltd', 				distance: 5.0, location:'', icon:'/img/hospitals/image4.jpg', desc:'private' },
-			{ name : 'MW Medical Pte Ltd', 						distance: 5.0, location:'', icon:'/img/hospitals/image5.jpg', desc:'' },
-			{ name : 'OmniMed Healthcare Holdings', 			distance: 5.0, location:'', icon:'/img/hospitals/image6.jpg', desc:'' },
-			{ name : 'Preciouz Kare Pte Ltd', 					distance: 5.0, location:'', icon:'/img/hospitals/image7.jpg', desc:'private' },
-			{ name : 'St Andrew\'s Community Hospital', 		distance: 5.0, location:'', icon:'/img/hospitals/image8.jpg', desc:'' },
-			{ name : 'Yong-En Care Centre', 					distance: 5.0, location:'', icon:'/img/hospitals/image9.jpg', desc:'' },
-			{ name : 'General Hospital', 					    distance: 5.0, location:'', icon:'/img/hospitals/image9.jpg', desc:'private' },
-		],
+        facilities : [
+            { name : 'AgeWell Artsz',                    distance: 5.0, location:'', icon:'/img/facilities/image1.jpg', desc:'' },
+            { name : 'Assist Care',                      distance: 5.0, location:'', icon:'/img/facilities/image2.jpg', desc:'' },
+            { name : 'Handicaps Welfare Association (Whampoa)', distance: 5.0, location:'', icon:'/img/facilities/image3.jpg', desc:'' },
+            { name : 'Lotus Eldercare Pte Ltd',             distance: 5.0, location:'', icon:'/img/facilities/image4.jpg', desc:'private' },
+            { name : 'MW Medical Pte Ltd',                    distance: 5.0, location:'', icon:'/img/facilities/image5.jpg', desc:'' },
+            { name : 'OmniMed Healthcare Holdings',          distance: 5.0, location:'', icon:'/img/facilities/image6.jpg', desc:'' },
+            { name : 'Preciouz Kare Pte Ltd',              distance: 5.0, location:'', icon:'/img/facilities/image7.jpg', desc:'private' },
+            { name : 'St Andrew\'s Community Hospital',       distance: 5.0, location:'', icon:'/img/facilities/image8.jpg', desc:'' },
+            { name : 'Yong-En Care Centre',                distance: 5.0, location:'', icon:'/img/facilities/image9.jpg', desc:'' },
+            { name : 'General Hospital',                   distance: 5.0, location:'', icon:'/img/facilities/image9.jpg', desc:'private' },
+        ],
 		parks : [
 			{ name : 'Dhoby Ghaut Green', 		distance: 5.0, location:'', icon:'/img/parks/park1.jpg', desc:'' },
 			{ name : 'Gardens by the Bay', 		distance: 5.0, location:'', icon:'/img/parks/park2.jpg', desc:'' },
@@ -111,18 +111,18 @@ app.controller('CareCtrl', function($rootScope, $scope, $http, $interval, SETTIN
 			{ name : 'Woodlands Town Garden', 	distance: 5.0, location:'', icon:'/img/parks/park9.jpg', desc:'' },
 			{ name : 'Pasir Ris Park', 			distance: 5.0, location:'', icon:'/img/parks/park10.jpg', desc:'' }
 		],
-		facilities : [
-			{ name : 'National Heart Centre', 				 distance: 5.0, location:'', icon:'/img/facilities/images-1.jpg', desc:'' },
-			{ name : 'National Neuroscience Institute', 	 distance: 5.0, location:'', icon:'/img/facilities/images-2.jpg', desc:'' },
-			{ name : 'Singapore National Eye Centre', 		 distance: 5.0, location:'', icon:'/img/facilities/images-3.jpg', desc:'' },
-			{ name : 'Raffles Hospital', 					 distance: 5.0, location:'', icon:'/img/facilities/images-4.jpg', desc:'' },
-			{ name : 'Ren Ci Hospital and Medicare Centre',  distance: 5.0, location:'', icon:'/img/facilities/images-5.jpg', desc:'' },
-			{ name : 'National University Hospital', 		 distance: 5.0, location:'', icon:'/img/facilities/images-6.jpg', desc:'' },
-			{ name : 'Tan Tock Seng Hospital', 				 distance: 5.0, location:'', icon:'/img/facilities/images-7.jpg', desc:'' },
-			{ name : 'KK Women\'s and Children\'s Hospital', distance: 5.0, location:'', icon:'/img/facilities/images-8.jpg', desc:'' },
-			{ name : 'Saint Jose\'s Community Hospital', 	 distance: 5.0, location:'', icon:'/img/facilities/images-9.jpg', desc:'' },
-			{ name : 'Saint Andrew\'s Community Hospital', 	 distance: 5.0, location:'', icon:'/img/facilities/images-10.jpg', desc:'' }
-		]
+        hospitals : [
+            { name : 'National Heart Centre',            distance: 5.0, location:'', icon:'/img/hospitals/images-1.jpg', desc:'' },
+            { name : 'National Neuroscience Institute',     distance: 5.0, location:'', icon:'/img/hospitals/images-2.jpg', desc:'' },
+            { name : 'Singapore National Eye Centre',      distance: 5.0, location:'', icon:'/img/hospitals/images-3.jpg', desc:'' },
+            { name : 'Raffles Hospital',                distance: 5.0, location:'', icon:'/img/hospitals/images-4.jpg', desc:'' },
+            { name : 'Ren Ci Hospital and Medicare Centre',  distance: 5.0, location:'', icon:'/img/hospitals/images-5.jpg', desc:'' },
+            { name : 'National University Hospital',       distance: 5.0, location:'', icon:'/img/hospitals/images-6.jpg', desc:'' },
+            { name : 'Tan Tock Seng Hospital',               distance: 5.0, location:'', icon:'/img/hospitals/images-7.jpg', desc:'' },
+            { name : 'KK Women\'s and Children\'s Hospital', distance: 5.0, location:'', icon:'/img/hospitals/images-8.jpg', desc:'' },
+            { name : 'Changi General Hospital',           distance: 5.0, location:'', icon:'/img/hospitals/images-9.jpg', desc:'' },
+            { name : 'Saint Andrew\'s Community Hospital',      distance: 5.0, location:'', icon:'/img/hospitals/images-10.jpg', desc:'' }
+        ]
 	};
 
 	$scope.show_template = '';
